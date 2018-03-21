83176f8b5a9d6fc4a444446cd54a86f60bc22527
rogercodes1 <roger.ucsb@gmail.com>
2018-03-21 17:37:56 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,38 @@
-class Dog
+require "pry"
+class Dog < ActiveRecord::Base
+
+  # attr_accessor :name , :breed, :id
+  # # attr_reader :id
+  # def Dog.create(hash)
+  #   name = hash[:name]
+  #   breed = hash[:breed]
+  #   # binding.pry
+  #   dog.name = name
+  #   dog.breed = breed
+  #   # binding.pry
+  #   dog.save
+  #   dog
+  # end
+  #
+  # def Dog.save(name, breed)
+  #   binding.pry
+  #   dog = Dog.save({name: name, breed: breed})
+  #   dog.save
+  #   dog
+  # end
+  #
+  # def Dog.update
+  #   binding.pry
+  #   # dog = Dog.create(name)
+  #   # dog.save
+  #   dog.update(name: name)
+  #
+  # end
+
+
+
+
 end
+
+
+#
